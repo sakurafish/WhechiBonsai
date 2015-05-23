1e8b939e3d4bff91ffd9533799a542ffcd1fc2b4
sakurafish <cckoyo173@gmail.com>
2015-05-23 19:06:58 +0800

    (8)介面精緻化
    1.封面圖固定
    2.滾輪滑動出現LOGO
    3.放作品圖片
    4.園主/園區介紹上的斜線(使用transform)

--- js/googlemap.js
+++ js/googlemap.js
@@ -6,10 +6,10 @@
                 // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
                 var mapOptions = {
                     // How zoomed in you want the map to start at (always required)
-                    zoom: 11,
+                    zoom: 18,
 
                     // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(21.9955,120.7455), // New York
+                    center: new google.maps.LatLng(21.9947862, 120.7459192), // New York
 
                     // How you would like to style the map. 
                     // This is where you would paste any style found on Snazzy Maps.
@@ -25,8 +25,15 @@
 
                 // Let's also add a marker while we're at it
                 var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(21.9955,120.7455),
+                    position: new google.maps.LatLng(21.9947862, 120.7459192),
                     map: map,
                     title: '董文吉名巨樹園'
                 });
+
             }
+
+
+
+
+
+
