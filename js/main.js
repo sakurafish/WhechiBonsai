1a59337a98c0be25df678a3f5606465613224b2d
sakurafish <cckoyo173@gmail.com>
2015-08-22 22:08:27 +0800

    RWD版

--- js/main.js
+++ js/main.js
@@ -1,106 +1,26 @@
-$(document).ready(function () {
-	
-	$("div[class^=\'img-bonsai']").bind("mouseover, mouseenter", function () {
-
-		$(this).find(".details").slideDown(100);
-
-	}).bind("mouseout, mouseleave", function () {
-		
-		$(this).find(".details").slideUp(100);
-
-
-	});
-	$("#tree2").bind("click, click", function () {
-
-		document.getElementById("tree2").innerHTML="關於董文吉";
-
-	}).bind("mouseover, mouseenter", function () {
-		
-		document.getElementById("tree2").innerHTML="關於名巨樹園";
-
-	});
+// slidedown
+	$(document).ready(function () {			
+			$("div[class^=\'works-img']").bind("mouseover, mouseenter", function () {
+				$(this).find(".works-view").slideDown(100);
+			}).bind("mouseout, mouseleave", function () {
+				$(this).find(".works-view").slideUp(100);
+			});
+			
 
 
-});
-
-		$('a[href^=\'#\']').click(function (e) {
-		    var yPos,  target = $($(this).attr('href') + ':first');
+	});	
+// Scoll
+	$('a[href^=\'#\']').click(function (e) {
+		    var star,  target = $($(this).attr('href') + ':first');
 		    e.preventDefault();
 
 		    if (target.length == 1) {
-		        yPos = target.offset().top;
+		        star = target.offset().top;
 		        if (window.matchMedia('(min-width: 1000px)').matches) {
-		            $('html,body').animate({ scrollTop: yPos - 60 }, 1000);
+		            $('html,body').animate({ scrollTop: star - 60 }, 1000);
 		        } else {
-		            $('html,body').animate({ scrollTop: yPos - 40 }, 1000);
+		            $('html,body').animate({ scrollTop: star - 40 }, 1000);
 		        }
 		    }
-		});
+		});	
 
-		var $content = $('.wrapper .frontcover')
-		  , wHeight  = $(window).height();
-
-		$(window).on('resize', function(){
-		  wHeight = $(window).height();
-		});
-
-		window.requestAnimFrame = (function()
-		{
-		  return  window.requestAnimationFrame       ||
-		          window.webkitRequestAnimationFrame ||
-		          window.mozRequestAnimationFrame    ||
-		          function( callback ){
-		            window.setTimeout(callback, 1000 / 60);
-		          };
-		})();
-
-		function Scroller()
-		{
-		  this.latestKnownScrollY = 0;
-		  this.ticking            = false;
-		}
-
-		Scroller.prototype = {
-		 
-		  init: function() {
-		    window.addEventListener('scroll', this.onScroll.bind(this), false);
-		    
-		  },
-
-
-		  onScroll: function() {
-		    this.latestKnownScrollY = window.scrollY;
-		    this.requestTick();
-		  },
-
-		  
-		  requestTick: function() {
-		    if( !this.ticking ) {
-		      window.requestAnimFrame(this.update.bind(this));
-		    }
-		    this.ticking = true;
-		  },
-
-		  update: function() {
-		    var currentScrollY = this.latestKnownScrollY;
-		    this.ticking       = false;
-		    
-		    
-		    
-		   if(currentScrollY > wHeight)
-		     $('.logo2').css({ position:"fixed", display:"block" });
-
-		   else
-		     $('.logo2').css({ position:"absolute", display:"none" });
-
-		   
-		    
-		   
-		  }
-		};
-
-		var scroller = new Scroller();  
-		scroller.init();
-
-
-	
